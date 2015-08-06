657a5572fb27250354a731aea1f7977214bf98ab
Jordan Potarf <jordan_potarf@baylor.edu>
2015-08-06 07:35:34 +0200

    moAdded run_analysis.py and instructions, simplified emap_to_tb_chanmanp.py

--- UserCode/H2TestBeamAnalyzer/emap_to_tb_chanmap.py
+++ UserCode/H2TestBeamAnalyzer/emap_to_tb_chanmap.py
@@ -5,13 +5,14 @@ import sys
 if len(sys.argv) != 2:
     print "Usage: ./emap_to_tb_chanmap.py [emap file]"
 else:
+    f = open('tb_chanmap.py', 'w')
     filename = sys.argv[1]
     with open(filename, 'r') as inFile:
         lines = inFile.readlines()
         i = 0
         reverse = []
-        print "chanmap = {}"
-        print "#ieta iphi depth channelId"
+        f.write("chanmap = {}\n")
+        f.write("#ieta iphi depth channelId\n")
         for line in lines:
             line = line[:-1]
 	    line = line.split("#")[0]
@@ -22,9 +23,10 @@ else:
                     ieta = channel[9]
                     iphi = channel[10]
                     depth = channel[11]
-                    print "chanmap[%s,%s,%s] = %s" % (ieta, iphi, depth, n)
+                    f.write("chanmap[%s,%s,%s] = %s\n" % (ieta, iphi, depth, n))
                     reverse.append(("chanmap[%s]  = (%s,%s,%s)" % (n, ieta, iphi, depth)))
             i = i + 1
-        print "\n"
+        f.write("\n")
         for rev in reverse:
-            print rev
+            f.write(rev)
+	    f.write("\n")
