11324c0858a5928f016066ac809d07e6222fc26a
Jay Dittmann <Jay_Dittmann@baylor.edu>
2015-08-24 11:45:25 +0200

    Fixed bug in tb_ana.py

--- UserCode/H2TestBeamAnalyzer/tb_ana.py
+++ UserCode/H2TestBeamAnalyzer/tb_ana.py
@@ -556,7 +556,7 @@ for ievt in xrange(start, start + nevts_to_run):
             
     fchan = {}
     fread = {}
-    for rchan in xrange(shbhe.numChs - 1):
+    for rchan in xrange(shbhe.numChs):
         print rchan
         test_chan = (shbhe.ieta[rchan], shbhe.iphi[rchan], shbhe.depth[rchan])
         if test_chan in chansToFind:
